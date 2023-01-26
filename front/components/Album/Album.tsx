40394ebecb0ccaa9725642644473d7bf0a00af80
Yun-Jehyeok <dbswpgur2@naver.com>
2023-01-26 14:47:12 +0900

    Album List Publishing

--- front/components/Album/Album.tsx
+++ front/components/Album/Album.tsx
@@ -1,4 +1,10 @@
+import { Carousel } from "antd";
 import ViewHeader from "components/ViewHeader";
+import Image from "next/image";
+import cat1 from "public/img/cat1.jpg";
+import dresden from "public/img/dresden.jpg";
+import friend from "public/img/friend1.jpg";
+import memories from "public/img/memories.jpg";
 
 const datas = [
   { id: 0, title: "Comfort Handy Craft" },
@@ -12,6 +18,12 @@ const datas = [
   { id: 8, title: "Comfort Handy Craft" },
   { id: 9, title: "Comfort Handy Craft" },
 ];
+const images = [
+  { id: 0, name: "cat1", src: cat1 },
+  { id: 1, name: "dresden", src: dresden },
+  { id: 2, name: "friend", src: friend },
+  { id: 3, name: "memories", src: memories },
+];
 
 export default function Album() {
   return (
@@ -24,25 +36,38 @@ export default function Album() {
       />
       <div className="w-full flex justify-center">
         <div className="w-default">
-          <div className="w-full h-11 mt-[124px] mb-36">
+          <div className="w-full h-11 mt-24 mb-32">
             <div className="font-josefin text-[22px] font-bold text-darkBlue">
-              Ecommerce Accesories & Fashion item
+              Our memories
             </div>
             <div className="font-lato text-xs text-subTextColor">
-              About 9,620 results
+              About 9,620 memories
             </div>
           </div>
           <div className="w-full flex gap-[48.5px] flex-wrap">
-            {datas.map((item) => (
-              <div key={item.id} className="w-[360px] h-[306px] mb-40">
-                <div className="w-full h-[270px] bg-slate-500"></div>
-                <div className="w-full h-9 flex justify-center flex-col">
-                  <div className="w-fit font-josefin text-darkBlue text-base border-b border-b-[#EEEFFB]">
-                    {item.title}
+            {datas.map((item) => {
+              return (
+                <div key={item.id} className="w-[360px] h-fit mb-16">
+                  <Carousel autoplay className="w-full h-[270px]">
+                    {images.map((item) => (
+                      <Image
+                        key={item.id}
+                        src={item.src}
+                        alt={item.name}
+                        width={360}
+                        height={270}
+                        className="rounded-md"
+                      />
+                    ))}
+                  </Carousel>
+                  <div className="w-full h-9 flex justify-center flex-col mt-1">
+                    <div className="w-fit font-josefin text-darkBlue text-base border-b-2 border-b-[#EEEFFB]">
+                      {item.title}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         </div>
       </div>
